908cebc3e34d2e6767571c0bed6534ced8e810dd
Ben Force <xdd741852963@gmail.com>
2022-07-04 21:07:09 +0800

    Fix #1 bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-cycle
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
