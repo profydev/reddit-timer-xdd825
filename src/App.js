0b9c642c029ed73e1a5d265dcebcd290fdf2dbc7
Ben Force <xdd741852963@gmail.com>
2022-07-04 20:49:44 +0800

    Add router direct to Home and Search pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import GlobalFonts from './fonts/fonts';
+// eslint-disable-next-line import/no-cycle
+import Home from './index';
+import Search from './Search';
 
 function App() {
   return (
     <div>
       <GlobalFonts />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
       App Placeholder
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root'),
+);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
