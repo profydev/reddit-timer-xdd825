30f242f17cf15b818e19cac33f0a914ecc7e2eb9
Ben Force <xdd741852963@gmail.com>
2022-07-05 13:05:00 +0800

    Change a bit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// eslint-disable-next-line import/no-cycle
 import App from './App';
-import Search from './Search';
 
 // eslint-disable-next-line react/jsx-filename-extension
 const root = document.getElementById('root');
 
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/search" element={<Search />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <App />,
   root,
 );
