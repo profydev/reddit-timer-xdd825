4f9dd06af827d142373e8e176688737fb519f370
Ben Force <47415067+xdd825@users.noreply.github.com>
2022-07-05 13:06:36 +0800

    Create app skeleton (#1)
    * Add Bitter and Montserrat font style
    
    * Add router direct to Home and Search pages
    
    * Add normalize.css
    
    * Fix #1 bug
    
    * Fix #2 problem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+import GlobalFonts from './fonts/fonts';
+// import Search from './Search';
+import 'normalize.css';
 
 function App() {
   return (
     <div>
+      <GlobalFonts />
+      {/* <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/search" element={<Search />} />
+      </Routes> */}
       App Placeholder
     </div>
   );
